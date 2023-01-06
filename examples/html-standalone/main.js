0546a9998d369fc2c28087a57672e960c0648d6f
foobar4423 <120440779+foobar4423@users.noreply.github.com>
2023-01-07 02:22:51 +0900

    Change example for chia reference wallet

--- examples/html-standalone/main.js
+++ examples/html-standalone/main.js
@@ -3,15 +3,35 @@ import { Web3Modal } from '@web3modal/standalone'
 
 // 0. Define ui elements
 const connectButton = document.getElementById('connect-button')
+const getWalletsButton = document.getElementById('getWallets-button')
+getWalletsButton.disabled = true
 
 // 1. Define constants
-const projectId = '8e6b5ffdcbc9794bf9f4a1952578365b'
+const projectId = '055e23cc494b99b68603d1225080660f'
 const namespaces = {
-  eip155: { methods: ['eth_sign'], chains: ['eip155:1'], events: ['accountsChanged'] }
+  chia: {
+    /*
+     * NG: chains: ['chia:testnet', 'chia:mainnet']
+     *
+     * The following error message is displayed in the Chia reference client (Developer Tools => Console).
+     * 'Non conforming namespaces. update() namespaces accounts don't satisfy requiredNamespaces chains for chia'
+     *
+     * The client returns only the main net, so the number of chains does not match and an error occurs.
+     *
+     * https://github.com/WalletConnect/walletconnect-monorepo/blob/2de7f690173a75665151d6a5ef76967748cff3ad/packages/utils/src/validators.ts#L450
+     *   https://github.com/WalletConnect/walletconnect-monorepo/blob/873f23556863f0467e4ea177541b82edee78066a/packages/utils/src/namespaces.ts#L3
+     *   https://github.com/WalletConnect/walletconnect-monorepo/blob/873f23556863f0467e4ea177541b82edee78066a/packages/utils/src/misc.ts#L165
+     * https://github.com/WalletConnect/walletconnect-monorepo/blob/873f23556863f0467e4ea177541b82edee78066a/packages/sign-client/src/controllers/engine.ts#L867
+     * https://github.com/WalletConnect/walletconnect-monorepo/blob/873f23556863f0467e4ea177541b82edee78066a/packages/sign-client/src/controllers/engine.ts#L147
+     */
+    chains: ['chia:mainnet'],
+    methods: ['chia_getWallets'],
+    events: []
+  }
 }
 
 // 3. Create modal client
-export const web3Modal = new Web3Modal({ projectId, standaloneChains: namespaces.eip155.chains })
+export const web3Modal = new Web3Modal({ projectId, standaloneChains: namespaces.chia.chains })
 export let signClient = undefined
 
 // 4. Initialise clients
@@ -28,6 +48,8 @@ async function initialize() {
 
 initialize()
 
+let session = undefined
+
 // 5. Create connection handler
 connectButton.addEventListener('click', async () => {
   try {
@@ -35,9 +57,35 @@ connectButton.addEventListener('click', async () => {
       const { uri, approval } = await signClient.connect({ requiredNamespaces: namespaces })
       if (uri) {
         web3Modal.openModal({ uri })
-        await approval()
+        session = await approval()
+        console.log(session)
+        getWalletsButton.disabled = false
+        getWalletsButton.innerText = 'Get Wallets'
         web3Modal.closeModal()
       }
+    }
+  } catch (err) {
+    console.error(err)
+  }
+})
+
+getWalletsButton.addEventListener('click', async () => {
+  try {
+    if (session) {
+      const [network, instance, fingerprint] = session.namespaces.chia.accounts[0].split(':')
+      const result = await signClient.request({
+        topic: session.topic,
+        chainId: `${network}:${instance}`,
+        request: {
+          id: 1,
+          jsonrpc: '2.0',
+          method: 'chia_getWallets',
+          params: {
+            fingerprint
+          }
+        }
+      })
+      console.log(result)
     }
   } catch (err) {
     console.error(err)
